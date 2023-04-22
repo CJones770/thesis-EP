a7d214249364e54fa69742cbb89c131437249fde
corey <corey@corey-MS-7B45.lan>
2023-04-22 02:26:13 -0700

    edit bmr script to use right function for bayesian model reduction

--- EP_scripts/analysis/mat/bmr.m
+++ EP_scripts/analysis/mat/bmr.m
@@ -1,5 +1,5 @@
 %Bayesian model reduction [depends on running prepDataStruct and prep_PEB first]
 
 %With ~1300 parameters, these take ~24-40 hours to complete each
-%bma_1 = spm_dcm_bma(PEB(1));
-bma_2 = spm_dcm_bma(PEB(2));
+%bma_1 = spm_dcm_bmr(PEB(1));
+bma_2 = spm_dcm_bmr(PEB(2));
