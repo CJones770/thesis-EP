eb295664ac43373352feb7b94af262bd28a46745
corey <corey@corey-MS-7B45.lan>
2023-04-24 11:29:56 -0700

    update remove_existing_outputs script to handle first frequency band filtered data and remove unnecessary syntax

--- EP_scripts/clearSpace/remove_existing_outputs.sh
+++ EP_scripts/clearSpace/remove_existing_outputs.sh
@@ -6,14 +6,16 @@ encdir=$4
 
 while read -r subject;
 do
-rm -r $2/$subject$3/$4/fMRI $5/$subject$3/$4/fMRI
-rm -r $2/$subject$3/$4/extracted_rois $5/$subject$3/$4/extracted_rois
-rm -r $2/$subject$3/$4/models $5/$subject$3/$4/models
-rm -r $2/$subject$3/$4/masks $5/$subject$3/$4/masks
-rm -r $2/$subject$3/$4/positions_of_MaxFs $5/$subject$3/$4/positions_of_MaxFs
-rm -r $2/$subject$3/$4/positions_of_MaxF2s $5/$subject$3/$4/positions_of_MaxF2s
-rm -r $2/$subject$3/$4/fMRI_filtered $5/$subject$3/$4/fMRI_filtered
-rm -r $2/$subject$3/$4/fMRI_f2 $5/$subject$3/$4/fMRI_f2
-rm -r $2/$subject$3/$4/fMRI_f2_filtered $5/$subject$3/$4/fMRI_f2_filtered
-rm -r $2/$subject$3/$4/fMRI_f2_filtered $5/$subject$3/$4/Logs
+rm -r $2/$subject$3/$4/Logs
+rm -r $2/$subject$3/$4/fMRI 
+rm -r $2/$subject$3/$4/extracted_rois 
+rm -r $2/$subject$3/$4/models 
+rm -r $2/$subject$3/$4/masks 
+rm -r $2/$subject$3/$4/positions_of_MaxFs 
+rm -r $2/$subject$3/$4/positions_of_MaxF2s 
+rm -r $2/$subject$3/$4/fMRI_filtered 
+rm -r $2/$subject$3/$4/fMRI_Filtered1
+rm -r $2/$subject$3/$4/fMRI_f2 
+rm -r $2/$subject$3/$4/fMRI_f2_filtered 
+rm -r $2/$subject$3/$4/fMRI_f2_filtered 
 done < $1
