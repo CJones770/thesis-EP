4c55fb6d588fccdb547f17636de2cf8e57d480e4
corey <corey@corey-MS-7B45.lan>
2023-05-02 20:38:56 -0700

    fix flipping over x-axis issue

--- EP_scripts/bin/mat/mm2vox.m
+++ EP_scripts/bin/mat/mm2vox.m
@@ -1,5 +1,5 @@
 function vox_pos = mm2vox(pos_mm)
-    vox_pos(1) = round((pos_mm(1) + 90) / 2);
+    vox_pos(1) = round((-pos_mm(1) + 90) / 2);
     vox_pos(2) = round((pos_mm(2) + 126) / 2);
     vox_pos(3) = round((pos_mm(3) + 72) / 2);
 end
