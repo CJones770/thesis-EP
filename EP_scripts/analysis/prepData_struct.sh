3cf1b0ab1df271473dcd399edc02babb7e578f2f
corey <corey@corey-MS-7B45.lan>
2023-09-10 14:42:26 -0700

    Update to include subject medication data when preparing matlab environment.

--- EP_scripts/analysis/prepData_struct.sh
+++ EP_scripts/analysis/prepData_struct.sh
@@ -4,4 +4,5 @@ subjectinfo=$2
 subjectdir=$3
 suffix=$4
 encdir=$5
-~/matlab/bin/matlab -r 'addpath(genpath("~/DPABI_V7.0_230110"));addpath(genpath("~/spm12"));cd("~/thesis-EP/EP_scripts/analysis/mat");pathList=string("'$subjectlist'"); pathInfo=string("'$subjectinfo'"); pathDir=string("'$subjectdir'");suff=string("'$suffix'");encdir=string("'$encdir'"); run load_DCMs.m;'
+medinfo=$6
+~/matlab/bin/matlab -r 'addpath(genpath("~/DPABI_V7.0_230110"));addpath(genpath("~/spm12"));cd("~/thesis-EP/EP_scripts/analysis/mat");pathList=string("'$subjectlist'"); pathInfo=string("'$subjectinfo'"); pathDir=string("'$subjectdir'");suff=string("'$suffix'");encdir=string("'$encdir'"); medInfo=string("'$medinfo'"); run load_DCMs.m;'
