62b28cf52253e6dde2577d557fcd34509bbf119d
corey <corey@corey-MS-7B45.lan>
2023-09-10 14:20:04 -0700

    Add matlab script that creates Discrete Cosine Transform basis sets.

---
+++ EP_scripts/initializors/DCT_define.m
@@ -0,0 +1,17 @@
+%spm filter implementation
+
+%Values for repition rate (TR) annd the highpass cutoff frequencies are adjustable 
+% Insert value of 1/f, f being the cutoff frequency in Hz to replace the values of 37 or 13.69, respectively.
+TR = 0.8;
+K_input = struct('RT',TR,'HParam',37,'row',ones(1,410));
+K = spm_filter(K_input);
+R = K.X0;
+
+save('/home/corey/thesis-EP/EP_scripts/DCT_basis_sets/K_Filter_DCT1_slow5.mat',"R");
+
+TR = 0.8;
+K_input = struct('RT',TR,'HParam',13.69,'row',ones(1,410));
+K = spm_filter(K_input);
+R = K.X0;
+
+save('/home/corey/thesis-EP/EP_scripts/DCT_basis_sets/K_Filter_DCT2_slow4.mat',"R");
