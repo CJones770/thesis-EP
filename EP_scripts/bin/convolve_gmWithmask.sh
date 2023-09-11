a57f61c4971650b6e9c4d7dec1aa1873ee5606a4
corey <corey@corey-MS-7B45.lan>
2023-09-10 17:41:27 -0700

    Update gray matter mask convolution script (to store only GM signal for correlation with various CSF signals). added notes on why in block comments exist.

--- EP_scripts/bin/convolve_gmWithmask.sh
+++ EP_scripts/bin/convolve_gmWithmask.sh
@@ -5,14 +5,19 @@ suffix=$3
 encdir=$4
 while read -r subject;
 do
-if [ ! -f $2/$subject$3/original_dcm_outputsPA/masks/resampled_gm_mask.nii.gz ]
-#if [ ! -f /media/corey/2_4TB-WDBlue/original_DCM_outputs/$subject$3/$4/masks/resampled_gm_mask.nii.gz ]
+#if [ ! -f $2/$subject$3/original_DCM_outputsPA/masks/resampled_gm_mask.nii.gz ]
+if [ ! -f /media/corey/2_4TB-WDBlue/original_DCM_outputs/$subject$3/$4/masks/resampled_gm_mask.nii.gz ]
 then
 echo "no resampeld grey matter mask available for subject $subject"
 continue
 fi
-fslmaths $2/$subject$3/$4/fMRI_filtered/Filtered_4DVolume.nii -mul $2/$subject$3/original_dcm_outputsPA/masks/resampled_gm_mask.nii.gz $2/$subject$3/$4/fMRI_filtered/gm_signal.nii.gz							
-fslmaths $2/$subject$3/$4/fMRI_f2_filtered/Filtered_4DVolume.nii -mul $2/$subject$3/original_dcm_outputsPA/masks/resampled_gm_mask.nii.gz $2/$subject$3/$4/fMRI_f2_filtered/gm_signal.nii.gz
+fslmaths $2/$subject$3/$4/fMRI_filtered/Filtered_4DVolume.nii -mul /media/corey/2_4TB-WDBlue/original_DCM_outputs/$subject$3/$4/masks/resampled_gm_mask.nii.gz $2/$subject$3/$4/fMRI_filtered/gm_signal.nii.gz
+
+#$2/$subject$3/original_dcm_outputsPA/masks/resampled_gm_mask.nii.gz $2/$subject$3/$4/fMRI_filtered/gm_signal.nii.gz							
+fslmaths $2/$subject$3/$4/fMRI_f2_filtered/Filtered_4DVolume.nii -mul /media/corey/2_4TB-WDBlue/original_DCM_outputs/$subject$3/$4/masks/resampled_gm_mask.nii.gz $2/$subject$3/$4/fMRI_f2_filtered/gm_signal.nii.gz
+
+#$2/$subject$3/original_dcm_outputsPA/masks/resampled_gm_mask.nii.gz $2/$subject$3/$4/fMRI_f2_filtered/gm_signal.nii.gz
 echo "$subject"
 done < $1
 
+#commented sections exist due to resampled masks being stored on separate mounts given near capacity disks at the time, simply serves to find information from all subjects even with the data being stored in separate places.
