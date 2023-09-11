529d0b6269232e8813d8646b243daf1cc2abe89f
corey <corey@corey-MS-7B45.lan>
2023-09-10 17:28:01 -0700

    Add script used to load DCM files into matlab environment when calling 'prepData_struct.m'.

--- EP_scripts/analysis/mat/load_DCMs.m
+++ EP_scripts/analysis/mat/load_DCMs.m
@@ -5,13 +5,14 @@ subject_num = str2double(subject_num_string{1,1});
 subject_num=transpose(subject_num);
 fclose(subjectlist);
 subTable = readtable(sprintf('%s',pathInfo));
+medTable = readtable(sprintf('%s',medInfo));
 num_subs=numel(subject_num);
 DCM_array = cell(num_subs,3);
 %Load DCM.mat files and store in cell array
 for idx=1:num_subs
     curr_sub=subject_num(idx);
-    DCM_array{idx,1} = open(sprintf("%s/%d%s/%s/models/glmNR/DCM_21Roi.mat",pathDir,curr_sub,suff,encdir));
-    DCM_array{idx,2} = open(sprintf("%s/%d%s/%s/models/glmNR2/DCM_21Roi.mat",pathDir,curr_sub,suff,encdir));
+    DCM_array{idx,1} = open(sprintf("%s/%d%s/%s/models/2glmNR/DCM_21Roi.mat",pathDir,curr_sub,suff,encdir));
+    DCM_array{idx,2} = open(sprintf("%s/%d%s/%s/models/2glmNR2/DCM_21Roi.mat",pathDir,curr_sub,suff,encdir));
     DCM_array{idx,3} = curr_sub;
 end
 
@@ -25,6 +26,8 @@ for idx=1:numel(subs_in_table)
     empty_sub = subs_in_table(idx);
     [ia, ~] = ismember(string(subTable.src_subject_id),string({empty_sub}));
     subTable(ia,:) = [];
+    [ia, ~] = ismember(string(medTable.src_subject_id),string({empty_sub}));
+    medTable(ia,:) = [];
     end
 end
 
@@ -33,8 +36,10 @@ end
 %In the subject information text file I am using
 %Replace 5 with the column your subject ID is in if appropriate
 sortedTable = sortrows(subTable,5);
+s_medTable = sortrows(medTable,5);
 sortedDCM = sortrows(DCM_array,3);
 
-joinedTable = horzcat(sortedDCM,sortedTable);
+infoTables = horzcat(sortedTable,s_medTable(:,10:14));
+joinedTable = horzcat(sortedDCM,infoTables);
 
 subs_in_table = joinedTable{:,"src_subject_id"};
