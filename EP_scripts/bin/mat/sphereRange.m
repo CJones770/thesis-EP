6496cfdb16e77839d85229d1025106e38c6099fb
corey <corey@corey-MS-7B45.lan>
2023-09-23 20:30:48 -0700

    Change variable name to accurately reflect diameter instead of radius. No functional change.

--- EP_scripts/bin/mat/sphereRange.m
+++ EP_scripts/bin/mat/sphereRange.m
@@ -1,8 +1,8 @@
-function ranges = sphereRange(vx_pos1,rad)
-    ranges(1) = vx_pos1(1)-(rad/2);
-    ranges(2) = vx_pos1(1)+(rad/2);
-    ranges(3) = vx_pos1(2)-(rad/2);
-    ranges(4) = vx_pos1(2)+(rad/2);
-    ranges(5) = vx_pos1(3)-(rad/2);
-    ranges(6) = vx_pos1(3)+(rad/2);
+function ranges = sphereRange(vx_pos1,diameter)
+    ranges(1) = vx_pos1(1)-(diameter/2);
+    ranges(2) = vx_pos1(1)+(diameter/2);
+    ranges(3) = vx_pos1(2)-(diameter/2);
+    ranges(4) = vx_pos1(2)+(diameter/2);
+    ranges(5) = vx_pos1(3)-(diameter/2);
+    ranges(6) = vx_pos1(3)+(diameter/2);
 end
